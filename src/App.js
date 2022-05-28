104c291474bf75e6715ecbdfd7b4075f4b38befc
maximemeshev <lemeshmah@gmail.com>
2022-05-28 13:40:21 +0300

    finish Timer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+import "./App.css";
+import { getPadTime } from "./helpers/getPadTime";
 
 function App() {
+  const [timeLeft, setTimeLeft] = useState(2 * 60);
+  const [isCouting, setIsCouting] = useState(false);
+
+  const minutes = getPadTime(Math.floor(timeLeft / 60));
+  const seconds = getPadTime(timeLeft - minutes * 60);
+
+  const handleStart = () => {
+    setIsCouting(true);
+    if (timeLeft === 0) setTimeLeft(5);
+  };
+  const handleStop = () => {
+    setIsCouting(false);
+  };
+  const handleReset = () => {
+    setIsCouting(false);
+    setTimeLeft(2 * 60);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      isCouting &&
+        setTimeLeft((timeleft) => (timeLeft >= 1 ? timeLeft - 1 : 0));
+    }, 1000);
+    if (timeLeft === 0) setIsCouting(false);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isCouting, timeLeft]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="timer">
+        <span>{minutes}</span>
+        <span>:</span>
+        <span>{seconds}</span>
+      </div>
+      <div className="buttons">
+        {isCouting ? (
+          <button onClick={handleStop}>Stop</button>
+        ) : (
+          <button onClick={handleStart}>Start</button>
+        )}
+
+        <button onClick={handleReset}>Reset</button>
+      </div>
     </div>
   );
 }
